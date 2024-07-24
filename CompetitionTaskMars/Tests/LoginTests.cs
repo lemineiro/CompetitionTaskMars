511ef2dfe976d4f541b7b0e9a30255842c368527
lemineiro <leticia.ltm@gmail.com>
2024-07-24 17:12:43 +1200

    First commit

---
+++ CompetitionTaskMars/Tests/LoginTests.cs
@@ -0,0 +1,16 @@
+namespace CompetitionTaskMars.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
